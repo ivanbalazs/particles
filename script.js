b2350ce7bf7dfb25c6464fba404e371ad47ab7dd
Balazs Ivan <balazs.ivan@kasa.com>
2023-08-17 01:35:32 +0200

    fill -> opaque

--- script.js
+++ script.js
@@ -74,7 +74,9 @@ function handleParticles() {
 }
 
 function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.02)';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
   handleParticles();
   requestAnimationFrame(animate);
 }
