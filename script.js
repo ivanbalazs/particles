2c1cbbe160c052cea7dbedc172e7b1e4fe18bac6
Balazs Ivan <balazs.ivan@kasa.com>
2023-08-17 02:07:55 +0200

    connections

--- script.js
+++ script.js
@@ -68,6 +68,21 @@ function handleParticles() {
   for (let i = 0; i < particlesArray.length; i++) {
     particlesArray[i].update();
     particlesArray[i].draw();
+    for (let j = i; j < particlesArray.length; j++) {
+      const dx = particlesArray[i].x - particlesArray[j].x;
+      const dy = particlesArray[i].y - particlesArray[j].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+      if (distance < 100) {
+        ctx.beginPath();
+        ctx.strokeStyle = particlesArray[i].color;
+        // ctx.lineWidth = particlesArray[i].size / 10;
+        ctx.lineWidth = 0.2;
+        ctx.moveTo(particlesArray[i].x, particlesArray[i].y);
+        ctx.lineTo(particlesArray[j].x, particlesArray[j].y);
+        ctx.stroke();
+        ctx.closePath();
+      }
+    }
     if (particlesArray[i].size <= 0.3) {
       particlesArray.splice(i, 1);
       i--;
@@ -76,6 +91,7 @@ function handleParticles() {
 }
 
 function animate() {
+  // ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   handleParticles();
