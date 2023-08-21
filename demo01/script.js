12adab7ffc5f0d11ff88ac5302b18d6327c7b682
Balazs Ivan <balazs.ivan@kasa.com>
2023-08-21 22:30:54 +0200

    MOved into subfolders

--- demo01/script.js
+++ demo01/script.js
@@ -91,9 +91,9 @@ function handleParticles() {
 }
 
 function animate() {
-  // ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
+  // ctx.fillRect(0, 0, canvas.width, canvas.height);
   handleParticles();
   hue += 2.5;
   requestAnimationFrame(animate);
