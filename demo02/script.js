b938085de2af96f3d2d3133a748394ac18948985
Balazs Ivan <balazs.ivan@kasa.com>
2023-08-21 22:51:18 +0200

    Gradient fillStyle

--- demo02/script.js
+++ demo02/script.js
@@ -5,20 +5,23 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-ctx.fillStyle = 'white';
+const gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
+gradient.addColorStop(0, 'white');
+gradient.addColorStop(0.5, 'magenta');
+gradient.addColorStop(1, 'blue');
+ctx.fillStyle = gradient;
 
 class Particle {
   constructor(effect) {
     this.effect = effect;
-    this.radius = Math.random() * 50 + 5;
+    this.radius = Math.random() * 10 + 5;
     this.x = this.radius + Math.random() * (this.effect.width - this.radius * 2);
     this.y = this.radius + Math.random() * (this.effect.height - this.radius * 2);
-    this.vx = Math.random() * 4 - 2;
-    this.vy = Math.random() * 4 - 2;
+    this.vx = Math.random() * 1 - 0.5;
+    this.vy = Math.random() * 1 - 0.5;
   }
   /** @param {CanvasRenderingContext2D} context */
   draw(context) {
-    context.fillStyle = `hsl(${this.x * 0.5}, 100%, 50%)`;
     context.beginPath();
     context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     context.fill();
